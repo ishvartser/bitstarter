6e2f433fc8636eceb524f51679e11a9bc5302f4e
Igor Shvartser <ishvarts@ucsc.edu>
2013-09-01 17:55:45 +0000

    Updates to web.js

--- web.js
+++ web.js
@@ -2,15 +2,9 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-fs.readFileSync (index.html, String {
-   if (err) throw err;
-   buf = new Buffer (256);
-   len = buf.write ('\u00bd', 0);
-   console.log ("msg: " + buf.toString ('utf-8', 0, len));
-});
-
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  var buf = fs.readFileSync ("index.html");
+  response.send(buf.toString ());
 });
 
 var port = process.env.PORT || 5000;
